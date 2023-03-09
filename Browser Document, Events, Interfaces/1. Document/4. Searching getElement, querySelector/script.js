6fb347364016d40de6000ff02becf104675e046d
electra777 <electra0789@gmail.com>
2023-03-09 22:51:05 +0300

    added task 4 from unit 1, part 2

---
+++ Browser Document, Events, Interfaces/1. Document/4. Searching getElement, querySelector/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let table = document.getElementById('age-table');
+let labels = table.getElementsByTagName('label');
+let td = table.querySelector('td');
+let formSearch = document.getElementsByName('search')[0];
+let inputFirst = formSearch.querySelector('input');
+let inputLast = formSearch.querySelectorAll('input')[inputs.length - 1];
+
+console.log(table);
+console.log(labels);
+console.log(td);
+console.log(formSearch);
+console.log(inputFirst);
+console.log(inputLast);
